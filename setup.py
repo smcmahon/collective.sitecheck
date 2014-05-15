3b67cee7d2e102ed63baed17db4e5d1f5a6e8af8
Steve McMahon <steve@dcn.org>
2014-05-15 16:15:55 -0700

    Fix readme reference

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.sitecheck',
       version=version,
       description="Site http check using sitemap",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
